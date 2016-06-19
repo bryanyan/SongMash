591da12002f80e3499edd5e981286afcf00bb0d8
Bryan Yan <bryanyan@andrew.cmu.edu>
2016-06-19 02:36:11 -0700

    add support for artist and song name w with chart lyric

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 var fs = require('fs');
 var path = require('path');
+var XML2JS = require('xml2js').parseString;
 var PythonShell = require('python-shell');
 var app = express();
 
@@ -11,12 +13,43 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 
 app.post('/submit_text', function(req, res) {
-	fs.writeFile('body.txt', req.body.texter, function(err) {
-		if(err) {
-			return console.log(err);
-		}
-		console.log("Successfully wrote to file!");
-	});
+	var givenText = '';
+	if (req.body.songName === '') {
+		var CHART_LYRIC_API = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist={artistName}&song={songName}';
+		var input = req.body.texter.split(',');
+		var artist = input[0];
+		var song = input[1];
+		CHART_LYRIC_API = CHART_LYRIC_API.replace('\{artistName\}', artist).replace('\{songName\}', song);
+		request(CHART_LYRIC_API, function (err, res, body) {
+			console.log("Searching up song lyrics on Chart Lyric...")
+			if(err) {
+				return console.log(err);
+			}
+			XML2JS(body, function (err, result) {
+				if (err) {
+					return console.log(err);
+				}
+				var jsonResponse = JSON.parse(JSON.stringify(result));
+				var lyrics = jsonResponse.GetLyricResult.Lyric;
+				givenText = lyrics[0];
+				fs.writeFile('body.txt', givenText, function(err) {
+					if(err) {
+						return console.log(err);
+					}
+					console.log("Successfully wrote to file!");
+				});
+			});
+		});
+	}
+	else {
+		givenText = req.body.texter;
+		fs.writeFile('body.txt', givenText, function(err) {
+			if(err) {
+				return console.log(err);
+			}
+			console.log("Successfully wrote to file!");
+		});
+	}
 	var options = {
   		mode: 'text',
 		pythonOptions: ['-u'],
