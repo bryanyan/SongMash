ad97b9b0fce9f3c14b97dd8be2cd19cea91084c6
Emily Newman <empatnewman@gmail.com>
2016-06-19 10:26:28 -0700

    finished script that adds captions and overwrites the original word clips with clips+captions

--- add_captions.py
+++ add_captions.py
@@ -1,3 +1,6 @@
+# Adds captions to the individual word clips.
+# Overwrites the original un-captioned clips
+
 import os
 from moviepy.editor import *
     
@@ -7,10 +10,11 @@ direct = os.path.join(current_path, "TED")
 direct = os.path.join(direct, "word_clips")
 
 for filename in os.listdir(direct):
-    if not filename == "lynne.mp4": continue
+    # if not filename == "lynne.mp4": continue
+    if filename[0] == ".": continue
     full_file = os.path.join(direct, filename)
     print full_file
     video = VideoFileClip(full_file)
     text = (TextClip(filename[:len(filename)-4], fontsize=36, color="white")).set_position(("center", "bottom")).set_duration(video.duration)
     result = CompositeVideoClip([video, text])
-    result.write_videofile(filename[:len(filename)-4] + "_test.mp4")
+    result.write_videofile(full_file, temp_audiofile="temp-audio.m4a", remove_temp=True, codec="libx264", audio_codec="aac")
