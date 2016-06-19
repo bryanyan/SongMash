7e6f506cb5829c207182b4c2105f32e0847146a2
Emily Newman <empatnewman@gmail.com>
2016-06-18 21:13:54 -0700

    fixed script 2

--- time_interpolation.py
+++ time_interpolation.py
@@ -3,8 +3,8 @@
 # Video id, person name, phrase, time start, duration
 # Output in the following: 
 # Video ID, person name, word, time start, length (estimated), isFirstWord 
-import string
 from __future__ import division
+import string
 import sys
 
 def readFile(path):
@@ -22,19 +22,20 @@ def parseCSV(path):
 def parse(content, result):
     lines = content.split("\n")
     for line_num in range(len(lines)):
+        if lines[line_num] == "": continue
         line_list = lines[line_num].split(", ")
         # List format: [video id, person name, phrase, time start, duration]
+        print line_list
         phrase = line_list[2]
         word_list = phrase.split(" ")
         for word_num in range(len(word_list)):
             new_line = [line_list[0], line_list[1], word_list[word_num]]
             duration = round(float(line_list[4]), 2)
-            time_start = rount(float(line_list[3]), 2)
+            time_start = round(float(line_list[3]), 2)
             length = duration/float(len(word_list)) #is a double
-            new_line.append(round(time_start + word_num*(length), 2))
-            new_line.append(round(length, 2))
-            if (word_num == 0): 
-                new_line.append(True)
+            new_line.append(str(round(time_start + word_num*(length), 2)))
+            new_line.append(str(round(length, 2)))
+            new_line.append(str(word_num == 0))
             assert(len(new_line) == 6)
             result.append(new_line)
     
@@ -42,11 +43,18 @@ def parse(content, result):
 
 
 def main():
-    if len(sys.argv) != 1:
+    if len(sys.argv) != 2:
         sys.exit("Must pass in exactly 1 file path")
-    path = sys.argv[0]
+    path = sys.argv[1]
     file_content = readFile(path)
-    result = [ ]
-    parse(file_content, result)
-    writeFile(path, result)
+    result_list = [ ]
+    parse(file_content, result_list)
+    result = ""
+    for line in result_list:
+        result = result + ", ".join(line) + "\n"
+    # yes i make assumptions such as the file name will end in .txt
+    # yes i'm the definition of bad practice
+    writeFile(path[:len(path)-4]+"_test.txt", result) # TODO temporary
+
+main()
     
