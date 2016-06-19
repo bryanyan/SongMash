3abbc81d70097ab429d763c099b207a96af2a63e
Emily Newman <empatnewman@gmail.com>
2016-06-18 23:10:05 -0700

    script2 now checks for files in directory called data that doesn't exist

--- time_interpolation.py
+++ time_interpolation.py
@@ -3,9 +3,16 @@
 # Video id, person name, phrase, time start, duration
 # Output in the following: 
 # Video ID, person name, word, time start, length (estimated), isFirstWord 
+
 from __future__ import division
 import string
 import sys
+import os
+
+
+
+
+
 
 def readFile(path):
     with open(path, "rt") as f:
@@ -20,42 +27,49 @@ def parse(content, result):
     for line_num in range(len(lines)):
         if lines[line_num] == "": continue
         line_list = lines[line_num].split(",")
-        # List format: [video id, person name, phrase, time start, duration]
+        # List format: [phrase, time start, duration]
         print line_list
-        phrase = line_list[2]
+        phrase = line_list[0]
         word_list = phrase.split(" ")
         for word_num in range(len(word_list)):
-            new_line = [line_list[0], line_list[1], word_list[word_num]]
-            duration = round(float(line_list[4]), 2)
-            time_start = round(float(line_list[3]), 2)
+            new_line = [word_list[word_num]]
+            duration = round(float(line_list[2]), 2)
+            time_start = round(float(line_list[1]), 2)
             length = duration/float(len(word_list)) #is a double
             new_line.append(str(round(time_start + word_num*(length), 2)))
             new_line.append(str(round(length, 2)))
             new_line.append(str(word_num == 0))
-            assert(len(new_line) == 6)
+            assert(len(new_line) == 4)
             result.append(new_line)
     
     return        
 
 
-def main():
-    if len(sys.argv) != 2:
-        sys.exit("Must pass in exactly 1 file path")
-    path = sys.argv[1]
-    file_content = readFile(path)
+def write(origin, destination):
+
+    file_content = readFile(origin)
     result_list = [ ]
     parse(file_content, result_list)
     result = ""
     for line in result_list:
         result = result + ",".join(line) + "\n"
-    # yes i make assumptions such as the file name will end in .txt
-    # yes i'm the definition of bad practice
-    writeFile(path[:len(path)-4]+"_test.csv", result) # TODO temporary
-    # TODO currently this just makes a new file with _test appended
-    # should probably make it so that it reads files and
-    # puts them in the right folders
+    writeFile(destination, result) # TODO temporary
     
     return
 
+
+# assumes there is a top-level directory named "data" containing everything
+def main():
+    checked = set()
+    for speaker in os.listdir("data/"):
+        # fetch files from here
+        origin = "data"+os.sep+speaker+os.sep+"phraseCSVs"
+        destination = "data"+os.sep+speaker+os.sep+"word_timing"
+        for data in os.listdir(origin):
+            filepath = os.path.join(origin, data)
+            if not (filepath in checked): 
+                write(filepath, os.path.join(destination, data))
+                checked.add(filepath)
+    return
+
 main()
-    
