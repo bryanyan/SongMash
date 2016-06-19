641213ac998505f2a335c205e7579db86f82db86
Scott Krulcik <skrulcik@andrew.cmu.edu>
2016-06-19 07:55:49 -0700

    Tab bars for song selection
    Also read body.txt asynchronously so it didn't race on its contents

--- app.js
+++ app.js
@@ -12,56 +12,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 
 
+var mergeBodyText = function(res) {
+    console.log("Beginning video merge...");
+    var options = {
+        mode: 'text',
+        pythonOptions: ['-u'],
+        args: ['body.txt']
+    };
+    PythonShell.run('merge.py', options, function(err, results) {
+        console.log('results: %j', results);
+        var file = __dirname + 'output.mp4';
+        res.download('output.mp4');
+    });
+}
+
 app.post('/submit_text', function(req, res) {
-	var givenText = '';
-	if (req.body.songName === '') {
-		var CHART_LYRIC_API = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist={artistName}&song={songName}';
-		var input = req.body.texter.split(',');
-		var artist = input[0];
-		var song = input[1];
-		CHART_LYRIC_API = CHART_LYRIC_API.replace('\{artistName\}', artist).replace('\{songName\}', song);
-		request(CHART_LYRIC_API, function (err, res, body) {
-			console.log("Searching up song lyrics on Chart Lyric...")
-			if(err) {
-				return console.log(err);
-			}
-			XML2JS(body, function (err, result) {
-				if (err) {
-					return console.log(err);
-				}
-				var jsonResponse = JSON.parse(JSON.stringify(result));
-				var lyrics = jsonResponse.GetLyricResult.Lyric;
-				givenText = lyrics[0];
-				fs.writeFile('body.txt', givenText, function(err) {
-					if(err) {
-						return console.log(err);
-					}
-					console.log("Successfully wrote to file!");
-				});
-			});
-		});
-	}
-	else {
-		givenText = req.body.texter;
-		fs.writeFile('body.txt', givenText, function(err) {
-			if(err) {
-				return console.log(err);
-			}
-			console.log("Successfully wrote to file!");
-		});
-	}
-	var options = {
-  		mode: 'text',
-		pythonOptions: ['-u'],
-		args: ['body.txt']
-	};
-	PythonShell.run('merge.py', options, function(err, results) {
-		console.log('results: %j', results);
-		var file = __dirname + 'output.mp4';
-		res.download('output.mp4');
-	});
+	var givenText = req.body.texter;
+    fs.writeFile('body.txt', givenText, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("Wrote custom text to body.txt");
+        mergeBodyText(res);
+    });
 });
 
+app.post('/submit_song', function(req, res) {
+	var givenText = '';
+    var CHART_LYRIC_API = 'http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist={artistName}&song={songName}';
+    var artist = req.body.artist;
+    var song = req.body.song;
+    CHART_LYRIC_API = CHART_LYRIC_API.replace('\{artistName\}', artist).replace('\{songName\}', song);
+    request(CHART_LYRIC_API, function (err, res, body) {
+        console.log("Searching up song lyrics on Chart Lyric...")
+        console.log("  Artist: ", artist, " Song: ", song);
+        if(err) {
+            return console.log(err);
+        }
+        XML2JS(body, function (err, result) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Received lyrics response...");
+            var jsonResponse = JSON.parse(JSON.stringify(result));
+            var lyrics = jsonResponse.GetLyricResult.Lyric;
+            givenText = lyrics[0];
+            fs.writeFile('body.txt', givenText, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log("Wrote custom text to body.txt");
+                mergeBodyText(res);
+            });
+        });
+    });
+});
 
 var server = app.listen(8080, function() {
 	var addr = server.address();
